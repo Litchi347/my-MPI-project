c8bc2b8058c40f3bf6469276a504b4607521b097
Litchi347 <liuxueer@LAPTOP-151G032O.localdomain>
2025-09-21 11:15:04 +0800

    完善了环境配置问题新增程序27的理解：同步模式的消息发送

--- code1_6/pro27.cpp
+++ code1_6/pro27.cpp
@@ -1,15 +1,17 @@
-# include<stdio.h>
+// 使用同步消息发送，
+// 发送进程分别以同步方式发送1个和4个数据，消息标识分别为1和2。
+// 接收进程用标准接收操作接收它，并检查接收到数据的个数
+# include <stdio.h>
 # include <mpi.h>
-# define SIZE 2
+# define SIZE 10
 
 static int src = 0;
 static int dest = 1;
 
 int main(int argc,char* argv[]){
     int act_size = 0;
-    int flag,rval;
     int buffer[SIZE];
-    MPI_Status status,status1,status2;
+    MPI_Status status1,status2;
     int count1,count2;
 
     MPI_Init(&argc,&argv);
@@ -24,8 +26,20 @@ int main(int argc,char* argv[]){
     act_size = 5;                                                              // 消息最大长度
     if(rank == src){                                                           // 当前进程为发送进程
         act_size = 1;
-        MPI_Ssend(buffer,act_size,MPI_INT,dest,1,MPI_COMM_WORLD);
-        
+        MPI_Ssend(buffer,act_size,MPI_INT,dest,1,MPI_COMM_WORLD);              // 同步消息发送，发送一个整数型，tag标识为1
+        fprintf(stderr,"MPI_Ssend %d data,tag=1\n",act_size);
+        act_size = 4;
+        MPI_Ssend(buffer,act_size,MPI_INT,dest,2,MPI_COMM_WORLD);              // 同步发送4个整数型，tag标识为2
+        fprintf(stderr,"MPI_Ssend %d data,tag=2\n",act_size);
+    }else if(rank == dest){                                                    // 当前进程为接收进程
+        MPI_Recv(buffer,act_size,MPI_INT,src,1,MPI_COMM_WORLD,&status1);       // 标准消息接收，这里指定的消息长度act_size是最大信息长度，tag为1
+        MPI_Recv(buffer,act_size,MPI_INT,src,2,MPI_COMM_WORLD,&status2);       // 标准消息接收，这里指定的消息长度act_size是最大信息长度，tag为2
 
+        MPI_Get_count(&status1,MPI_INT,&count1);
+        fprintf(stderr,"receive %d data,tag=%d\n",count1,status1.MPI_TAG);     // 消息1包含的数据的个数
+        MPI_Get_count(&status2,MPI_INT,&count2);
+        fprintf(stderr,"receive %d data,tag=%d\n",count2,status2.MPI_TAG);     // 消息2包含的数据个数
     }
+    MPI_Finalize();
+    return 0;
 }
