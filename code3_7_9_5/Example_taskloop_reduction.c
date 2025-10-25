12ab60c67fb56fae3891ac63a20e47492e7fa96f
Litchi347 <liuxueer@LAPTOP-151G032O.localdomain>
2025-10-25 11:36:30 +0800

    明确的 task 和 taskloop、reduction 和 in_reduction的区别

--- code3_7_9_5/Example_taskloop_reduction.c
+++ code3_7_9_5/Example_taskloop_reduction.c
@@ -1,10 +1,6 @@
-// 以下两个示例展示了如何通过两种不同方式使用taskloop reduction实现数组归约
-// 第一个示例将 reduction 子句应用于 taskloop 结构体。任务归约分为两个组成部分：由taskgroup区域定义的归约作用域，以及参与归约的任务
-// 本示例中，reduction 子句同时定义了两者语义：
-// 首先指明与 taskloop 结构体关联的隐式 taskgroup 区域作为归约作用域；
-// 其次将 taskloop 创建的所有任务定义为归约参与者
-// 关于第一个特性需特别注意，若向taskloop结构体添加 nogroup 子句将导致代码非合规——本质上是由于存在一组参与未定义归约的任务
+// 以下两个示例展示了如何通过两种不同方式使用 taskloop reduction 实现数组归约
 # include <stdio.h>
+# define N 100
 
 int array_sum(int n,int *v){
     int i;
@@ -33,18 +29,76 @@ int array_sum(int n,int *v){
     return res;
 }
 
-int main(int arvc,char *argv[])
-{
-    int n = 10;
-    int v[10] = {1,2,3,4,5,6,7,8,9,10};
+// int main(int arvc,char *argv[])
+// {
+//     int n = 10;
+//     int v[10] = {1,2,3,4,5,6,7,8,9,10};
 
-    #pragma omp parallel
+//     #pragma omp parallel
 
-    // 只有一个线程执行，其他线程跳过
-    #pragma omp single
-    {
-        int res = array_sum(n,v);
-        printf("The result is %d\n",res);
+//     // 只有一个线程执行，其他线程跳过
+//     #pragma omp single
+//     {
+//         int res = array_sum(n,v);
+//         printf("The result is %d\n",res);
+//     }
+//     return 0;
+// }
+
+
+// task 和 taskloop
+// reduction 和 in_reduction
+int main(){
+    int a[N],asum = 0;
+
+    // 初始化
+    for(int i = 0;i < N;i++){
+        a[i] = i;
     }
-    return 0;
+
+    // taskloop reductions
+    #pragma omp parallel master
+    #pragma omp taskloop reduction(+:asum)                   // taskloop 1
+                                                             // 主线程将循环分成若干个任务（task 只表创建一个任务）
+    for(int i = 0;i < N;i ++){
+        asum += a[i];
+    }
+
+    #pragma omp parallel reduction(task,+:asum)              // 所有带 in_reduction(+:asum) 的任务、taskloop 的局部和都属于这个归约集合
+    {
+        #pragma omp master                                   // 第一个部分，没有 in_reduction(+:asum) ，直接访问主线程的 asum
+
+        #pragma omp task in_reduction(+:asum)                // task 2
+        for(int i = 0;i < N;i++){
+            asum += a[i];
+        }
+
+        #pragma omp master taskloop in_reduction(+:asum)     // taskloop 2
+                                                             // 属于上层的 reduction(task,+:asum)，每个任务计算自己的局部和，最终在并行区域结束时归约到主线程的 asum
+        for(int i = 0; i < N;i ++){
+            asum += a[i];
+        }
+    }
+
+    // taskloop simd reductions
+    #pragma omp parallel master
+    #pragma omp taskloop simd reduction(+:asum)              // taskloop simd 3
+                                                             // 加入 simd ，每个任务内部的循环使用向量化
+    for(int i = 0;i < N; i++){
+        asum += a[i];
+    }
+
+    #pragma omp parallel reduction(task,+:asum)
+    {
+        #pragma omp master                                   // 主线程创建任务
+        #pragma omp task in_reduction(+:asum)                // task 4 ，其他线程分配任务并参加归约
+        for(int i = 0;i < N;i ++){
+            asum += a[i];
+        }
+        #pragma omp master taskloop simd in_reduction(+:asum)// taskloop simd 4
+        for(int i = 0;i < N;i ++){
+            asum += a[i];
+        }
+    }
+    printf("asum=%d\n",asum);
 }
